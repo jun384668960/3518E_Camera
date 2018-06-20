e0f35681ed9ac0fdb2aec8f699e3af8117f7bf43
jun384668960 <Donyj_ljj@163.com>
2018-06-20 16:18:47 +0800

    合理的停止一个录像文件

--- trunk/record/main.cpp
+++ trunk/record/main.cpp
@@ -9,12 +9,10 @@
 #include "utils_log.h"
 #include "utils_common.h"
 
+#define	EN_AUDIO	0
 int g_W = 1280;
 int g_H = 720;
-uint8_t g_spspps[] = {
-	0x00,0x00,0x00,0x01,0x67,0x42,0x00,0x1f,0x9d,0xa8,0x14,0x01,0x6e,0x9b,0x80,0x80,0x80,0x81,
-	0x00,0x00,0x00,0x01,0x68,0xce,0x3c,0x80
-};
+
 MP4EncoderResult AddH264Track(MP4Encoder &encoder, uint8_t *sData, int nSize)
 {
 	return encoder.MP4AddH264Track(sData, nSize, g_W, g_H);
@@ -43,13 +41,19 @@ MP4EncoderResult WriteALawData(MP4Encoder &encoder, uint8_t *sData, frame_info i
 
 int main(int argc, const char *argv[])
 {
-	
-	
+	#if EN_AUDIO
 	shm_stream_t* audio_stream = shm_stream_create("rec_audioread", "audiostream", STREAM_MAX_USER, STREAM_MAX_FRAMES, STREAM_AUDIO_MAX_SIZE, SHM_STREAM_READ);
+	#endif
 	shm_stream_t* main_stream = shm_stream_create("rec_mainread", "mainstream", STREAM_MAX_USER, STREAM_MAX_FRAMES, STREAM_VIDEO_MAX_SIZE, SHM_STREAM_READ);
 	unsigned char* pData = (unsigned char*)malloc(200*1024);
 	while(1)
 	{
+		bool Iwait = false;
+		bool IwaitOver = false;
+		bool vTrackSet = false;
+		#if EN_AUDIO
+		bool aTrackSet = false;
+		#endif
 		MP4EncoderResult result;
 		MP4Encoder encoder;
 		//根据当前时间取得文件名
@@ -57,9 +61,8 @@ int main(int argc, const char *argv[])
 		localtime_mp4name_get(filename);
 		LOGI_print("start recording file %s", filename);
 		
-		result = encoder.MP4CreateFile(filename, 55);
-		result = AddH264Track(encoder, g_spspps, sizeof(g_spspps)/sizeof(g_spspps[0]));
-		result = AddALawTrack(encoder);
+		result = encoder.MP4CreateFile(filename, 15);
+		
 		while(1)
 		{
 			frame_info info;
@@ -68,38 +71,83 @@ int main(int argc, const char *argv[])
 			int ret = shm_stream_get(main_stream, &info, &frame, &length);
 			if(ret == 0)
 			{
-				memcpy(pData, frame, length);
-				result = WriteH264Data(encoder, pData, info);
-				shm_stream_post(main_stream);
-				if(result != MP4ENCODER_ENONE)
+				//如果还没有取得I帧，并且当前非I帧
+				if(!Iwait && info.key == 1)
 				{
-					LOGW_print("WriteH264Data error:%d", result);
-					break;
+					LOGW_print("Wait for I frame Iwait:%d info.key:%d", Iwait, info.key);
+					shm_stream_post(main_stream);
 				}
-
+				else
+				{
+					if(IwaitOver && info.key == 5)
+					{
+						LOGW_print("WriteH264Data IwaitOver:%d", IwaitOver);
+						break;
+					}
+						
+					
+					memcpy(pData, frame, length);
+					Iwait = true;
+					if(!vTrackSet)
+					{
+						result = AddH264Track(encoder, pData, length);
+						LOGW_print("AddH264Track error:%d", result);
+						vTrackSet = true;
+						#if EN_AUDIO
+						result = AddALawTrack(encoder);
+						LOGW_print("AddALawTrack error:%d", result);
+						aTrackSet = true;
+						#endif
+					}
+						
+					result = WriteH264Data(encoder, pData, info);
+					shm_stream_post(main_stream);
+					if(result == MP4ENCODER_ERROR(MP4ENCODER_WARN_RECORD_OVER))
+					{	
+						//直到下一个I帧出现
+						IwaitOver = true;
+					}
+					else if(result != MP4ENCODER_ENONE)
+					{
+						LOGW_print("WriteH264Data error:%d", result);
+						break;
+					}
+				}
+				
+				#if EN_AUDIO
 				while(1)
 				{
+					
 					ret = shm_stream_get(audio_stream, &info, &frame, &length);
 					if(ret == 0)
 					{
-						memcpy(pData, frame, length);
-						result = WriteALawData(encoder, pData, info);
-						shm_stream_post(audio_stream);
-						if(result != MP4ENCODER_ENONE)
+						if(!aTrackSet)
 						{
-							LOGW_print("WriteALawData error:%d", result);
-							break;
+							shm_stream_post(audio_stream);
 						}
+						else
+						{
+							memcpy(pData, frame, length);
+							result = WriteALawData(encoder, pData, info);
+							shm_stream_post(audio_stream);
+							if(result != MP4ENCODER_ENONE)
+							{
+								LOGW_print("WriteALawData error:%d %d", result, shm_stream_remains(audio_stream));
+//								break;
+							}
+						}
+
 					}
 					else
 					{
 						break;
 					}
 				}
+				#endif
 			}
 			else
 			{
-				usleep(20*1000);
+				usleep(5*1000);
 			}
 		}
 		
@@ -109,7 +157,9 @@ int main(int argc, const char *argv[])
 	free(pData);
 	if(main_stream != NULL)
 		shm_stream_destory(main_stream);
+	#if EN_AUDIO
 	if(audio_stream != NULL)
 		shm_stream_destory(audio_stream);
+	#endif
 	return 0;
 }
